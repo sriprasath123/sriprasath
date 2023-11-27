dfb01f7f758ebe9d311100b4f3602864adbf9d3e
sriprasath123 <146945241+sriprasath123@users.noreply.github.com>
2023-11-27 21:14:34 +0530

    Add files via upload

---
+++ Average_number.py
@@ -0,0 +1,7 @@
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers)
+
+# Example
+numbers = [1, 2, 3, 4, 5]
+average = calculate_average(numbers)
+print(average)  # Output: 3.0
---
+++ power_number.py
@@ -0,0 +1,6 @@
+def power(base, exponent):
+    return base ** exponent
+
+# Example
+result = power(2, 3)
+print(result)  # Output: 8
